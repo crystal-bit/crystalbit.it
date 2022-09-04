e547ced0affb7acbf60920720ce70f4d11e5de27
Davide Cristini <davcri91@gmail.com>
2022-09-04 17:07:07 +0200

    feat: many improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "ignore",
   output: "static",
+  integrations: [mdx(), image()]
 });
--- config.ts
+++ config.ts
@@ -1,5 +1,11 @@
 export const site = {
   title: "Crystal Bit",
+  menu: [
+    { name: "about", sectionSlug: "about" },
+    { name: "guide", sectionSlug: "guide" },
+    { name: "blog", sectionSlug: "posts" },
+    { name: "supporter", sectionSlug: "supporter" },
+  ],
 };
 
 export const externalLinks = {
